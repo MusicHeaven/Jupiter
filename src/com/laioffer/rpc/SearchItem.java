8145851d105ee48bfbd40a9ec4d6fa1214779cde
mengxuan <kit.mma@gmail.com>
2019-12-20 23:20:06 -0800

    First commit

---
+++ src/com/laioffer/rpc/SearchItem.java
@@ -0,0 +1,82 @@
+package com.laioffer.rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.MySQLConnection;
+import entity.Item;
+import external.TicketMasterClient;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+    	HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.setStatus(403);
+			return;
+		}
+		
+		String userId = session.getAttribute("user_id").toString();
+
+		// String userId = request.getParameter("user_id");
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+
+		TicketMasterClient client = new TicketMasterClient(); 
+		List<Item> items = client.search(lat, lon, null);
+
+		MySQLConnection connection = new MySQLConnection();
+		Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+		connection.close();
+
+		JSONArray array = new JSONArray();
+		for (Item item : items) {
+			JSONObject obj = item.toJSONObject();
+			try {
+				obj.put("favorite", favoritedItemIds.contains(item.getItemId()));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			array.put(obj);
+		}
+		RpcHelper.writeJsonArray(response, array);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse    esponse) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// doGet(request, response);
+	}
+}
---
+++ src/db/MySQLDBUtil.java
@@ -0,0 +1,14 @@
+package db;
+
+public class MySQLDBUtil {
+	private static final String INSTANCE = "laiproject-instance.cfoxiswfe0ot.us-east-2.rds.amazonaws.com";
+	private static final String PORT_NUM = "3306";
+	public static final String DB_NAME = "laiproject";
+	private static final String USERNAME = "laiprojectadmin";
+	private static final String PASSWORD = "laiprojectadmin";
+	public static final String URL = "jdbc:mysql://"
+			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
+			+ "?user=" + USERNAME + "&password=" + PASSWORD
+			+ "&autoReconnect=true&serverTimezone=UTC";
+
+}
